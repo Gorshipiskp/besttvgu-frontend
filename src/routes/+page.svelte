cf85d07a3096ff0045b97b514c69313b6ef6aa9f
Gorshipisk <107960553+Gorshipiskp@users.noreply.github.com>
2025-09-07 14:49:32 +0300

    Add files via upload

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script type="text/javascript">
+    let newScript = document.createElement('script');
+
+    newScript.setAttribute('src', 'https://telegram.org/js/telegram-widget.js?22');
+    newScript.setAttribute('data-telegram-login', 'best_debug_bot');
+    newScript.setAttribute('data-size', 'large');
+    newScript.setAttribute('data-onauth', 'onTelegramAuth(user)');
+    newScript.setAttribute('data-request-access', 'write');
+    document.head.appendChild(newScript);
+
+    function onTelegramAuth(user) {
+        alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+    }
+</script>
+
+<main>
+    <div id="telegram-widget"></div>
+</main>
