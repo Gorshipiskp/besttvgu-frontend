642a539912e94995352f7d00d20ba5063010aef4
Gorshipisk <107960553+Gorshipiskp@users.noreply.github.com>
2025-09-07 13:46:43 +0300

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script type="text/javascript">
+    let newScript = document.createElement('script');
+
+    newScript.setAttribute('src', 'https://telegram.org/js/telegram-widget.js?22');
+    newScript.setAttribute('data-telegram-login', 'best_debug_bot');
+    newScript.setAttribute('data-size', 'large');
+    newScript.setAttribute('data-onauth', 'onTelegramAuth(user)');
+    newScript.setAttribute('data-request-access', 'write');
+    document.head.appendChild(newScript);
+
+    function onTelegramAuth(user) {
+        alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+    }
+</script>
+
+<main>
+    <div id="telegram-widget"></div>
+</main>
